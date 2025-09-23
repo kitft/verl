40bbcd6e7893e0a8ca9fdb75467d16acf202eb9f
Kit FT <kitfrasertaliente@gmail.com>
2025-09-23 20:15:40 +0000

    update

--- verl/nla/workers/__init__.py
+++ verl/nla/workers/__init__.py
@@ -1,8 +1,9 @@
 """NLA worker implementations."""
 
-from .nla_actor_worker import NLAActorWorker, create_nla_actor_worker
+from .nla_actor_worker import NLAActorRolloutRefWorker
+from .nla_critic_worker import NLACriticWorker
 
 __all__ = [
-    "NLAActorWorker",
-    "create_nla_actor_worker",
+    "NLAActorRolloutRefWorker",
+    "NLACriticWorker",
 ]
